59d06f9c304beeee8727520844a9ff5c3dd0c288
J. Simon Richard <jsimonrichard@gmail.com>
2025-03-29 12:55:45 -0400

    Use PyTest

---
+++ src/python/test/test_client.py
@@ -0,0 +1,45 @@
+import pytest
+from lean_sdk import LeanRpcClient, LeanSession
+from pathlib import Path
+import logging
+
+
+def pytest_configure(config):
+    # Set the logging level to DEBUG
+    logging.basicConfig(level=logging.DEBUG)
+
+
+@pytest.fixture
+def client():
+    lean_project_path = Path(__file__).parent.parent.parent / "LeanSDK"
+    return LeanRpcClient(lean_project_path)
+
+
+@pytest.fixture
+def session(client):
+    return LeanSession(client)
+
+
+def test_client(session):
+    res = session.run_command("#check id")
+    assert res == [
+        {
+            "severity": "info",
+            "pos": {"line": 1, "column": 0},
+            "endPos": {"line": 1, "column": 6},
+            "data": "id.{u} {α : Sort u} (a : α) : α",
+        }
+    ]
+
+
+def test_imports(client):
+    session = LeanSession(client, imports=["LeanSDK.JsonRpc"])
+    res = session.run_command("#check IO.FS.Stream.writeJsonRpcMessage")
+    assert res == [
+        {
+            "severity": "info",
+            "pos": {"line": 1, "column": 0},
+            "endPos": {"line": 1, "column": 6},
+            "data": "IO.FS.Stream.writeJsonRpcMessage (h : IO.FS.Stream) (msg : Lean.JsonRpc.Message) : IO Unit",
+        }
+    ]
